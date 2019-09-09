5cc3719bb696921e3608ae5efbeec8a017958194
AbrahamTesla <manlangitv@gmail.com>
2019-09-08 23:56:49 -0400

    modifications

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,22 +3,20 @@ import { Grid, Paper, Typography } from "@material-ui/core";
 
 const VideoItem = ({ listOfVideo, onVideoSelect }) => {
   return (
-    <Grid container spacing={12}>
-      <Grid item xs={12}>
-        <Paper
-          style={{ display: "flex", alignItems: "center", cursor: "pointer" }}
-          onClick={() => onVideoSelect(listOfVideo)}
-        >
-          <img
-            style={{ marginRight: "20px" }}
-            alt="thumbnail"
-            src={listOfVideo.snippet.thumbnails.medium.url}
-          />
-          <Typography variant="subtitle1">
-            <b>{listOfVideo.snippet.title}</b>
-          </Typography>
-        </Paper>
-      </Grid>
+    <Grid item xs={12}>
+      <Paper
+        style={{ display: "flex", alignItems: "center", cursor: "pointer" }}
+        onClick={() => onVideoSelect(listOfVideo)}
+      >
+        <img
+          style={{ marginRight: "20px" }}
+          alt="thumbnail"
+          src={listOfVideo.snippet.thumbnails.medium.url}
+        />
+        <Typography variant="subtitle1">
+          <b>{listOfVideo.snippet.title}</b>
+        </Typography>
+      </Paper>
     </Grid>
   );
 };
